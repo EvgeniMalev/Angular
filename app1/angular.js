0927fd3dd77d06b3156853e1647373e9345059ab
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-10 23:58:51 +0200

    Create angular.js

---
+++ app1/angular.js
@@ -0,0 +1,16 @@
+{
+  "projects": {
+    "my-angular-app": {
+      "architect": {
+        "build": {
+          "options": {
+            "styles": [
+              "src/styles.css"
+            ],
+            "scripts": []
+          }
+        }
+      }
+    }
+  }
+}
