83c7627989588242bfa82cd2b983067f68121890
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-05-06 22:41:27 +0300

    Create lanciabeta.component.ts

---
+++ Workshop: Modules and Routing/Lancia app/src/app/lanciabeta/lanciabeta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lanciabeta',
+  templateUrl: './lanciabeta.component.html',
+  styleUrl: './lanciabeta.component.css'
+})
+export class LanciabetaComponent {
+
+}
