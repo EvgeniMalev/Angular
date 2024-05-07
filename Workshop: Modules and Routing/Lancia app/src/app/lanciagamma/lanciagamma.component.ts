0ceb72f6778955cead93251b022573e02057a786
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-05-08 00:06:40 +0300

    Create lanciagamma.component.ts

---
+++ Workshop: Modules and Routing/Lancia app/src/app/lanciagamma/lanciagamma.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lanciagamma',
+  templateUrl: './lanciagamma.component.html',
+  styleUrl: './lanciagamma.component.css'
+})
+export class LanciagammaComponent {
+
+}
